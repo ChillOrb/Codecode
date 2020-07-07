51333ab7bdc535552970692d0507eb258b9aadda
Rakshit Parashar <34675136+rishu2403@users.noreply.github.com>
2020-07-07 11:45:59 -0700

    Update tribonacci.py

--- LeetCode/tribonacci.py
+++ LeetCode/tribonacci.py
@@ -1,1 +1,16 @@
 
+# https://leetcode.com/problems/n-th-tribonacci-number/
+# using DP , hash map
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        hmap = {0: 0, 1: 1, 2: 1}
+        def trib(n):
+            if n in hmap:
+                return hmap.get(n)
+            else:
+                value = trib(n-1) + trib(n-2) + trib(n-3)
+                hmap[n] = value
+                return value
+        return trib(n)
