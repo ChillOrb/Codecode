233469f6b538ee0084297245e50756bc57aeef16
Rakshit Parashar <34675136+rishu2403@users.noreply.github.com>
2020-08-08 20:27:37 -0700

    Create Zeroes.py

---
+++ CtCi/ArraysStrings/Zeroes.py
@@ -0,0 +1,29 @@
+def zero_matrix(matrix):
+    m = len(matrix)
+    n = len(matrix[0])
+    rows = []
+    cols = []
+
+    for x in range(m):
+        for y in range(n):
+            if matrix[x][y] == 0:
+                rows.append(x)
+                cols.append(y)
+
+    for row in rows:
+        nullify_row(matrix, row)
+
+    for col in cols:
+        nullify_col(matrix, col)
+
+    return matrix
+
+
+def nullify_row(matrix, row):
+    for i in range(len(matrix[0])):
+        matrix[row][i] = 0
+
+
+def nullify_col(matrix, col):
+    for i in range(len(matrix)):
+        matrix[i][col] = 0
