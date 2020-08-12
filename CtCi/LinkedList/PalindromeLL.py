6dfd87a64484c9b6a908a038aa3a427d287f3581
Rakshit Parashar <34675136+rishu2403@users.noreply.github.com>
2020-08-12 09:39:49 -0700

    Update PalindromeLL.py

--- CtCi/LinkedList/PalindromeLL.py
+++ CtCi/LinkedList/PalindromeLL.py
@@ -1,3 +1,52 @@
+
+#O(N) time and O(1) space best solution 
+
+
+def isPalindrome(self, head):
+    # rev records the first half, need to set the same structure as fast, slow, hence later we have rev.next
+    rev = None
+    # initially slow and fast are the same, starting from head
+    slow = fast = head
+    while fast and fast.next:
+        # fast traverses faster and moves to the end of the list if the length is odd
+        fast = fast.next.next
+        
+        # take it as a tuple being assigned (rev, rev.next, slow) = (slow, rev, slow.next), hence the re-assignment of slow would not affect rev (rev = slow)
+        rev, rev.next, slow = slow, rev, slow.next
+    if fast:
+       # fast is at the end, move slow one step further for comparison(cross middle one)
+        slow = slow.next
+    # compare the reversed first half with the second half
+    while rev and rev.val == slow.val:
+        slow = slow.next
+        rev = rev.next
+    
+    # if equivalent then rev become None, return True; otherwise return False 
+    return not rev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def is_palindrome(self):
         # Method 1:
         # s = ""
