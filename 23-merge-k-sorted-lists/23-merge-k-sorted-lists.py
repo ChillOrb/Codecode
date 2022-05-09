43cb5a3e1d5283722d3e87c01f311227838c4d01
Rakshit Parashar <34675136+rishu2403@users.noreply.github.com>
2022-05-08 18:32:18 -0700

    Time: 88 ms (99.50%), Space: 18.4 MB (20.82%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,47 +5,21 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def mergeTwoLists(l1,l2):
-                dummy = node = ListNode()
-                if l1==None and l2==None:
-                    
-                    return None
-                if l1 is None:
-                    node=l2
-                    return node
-                elif l2 is None:
-
-                    node=l1
-                    return node        
-                while l1 and l2:
-
-                    if l1.val == l2.val:
-                        node.next=l1
-                        node=node.next
-                        l1=l1.next
-                    elif l1.val<l2.val:
-                        node.next=l1
-                        node=node.next
-                        l1=l1.next
-                    else:
-                        node.next=l2
-                        node=node.next
-                        l2=l2.next
-
-                if l1==None:
-                    node.next=l2
-                else:
-                    node.next=l1
-
-                return dummy.next
-
-
-        if len(lists)==0:
-            return None
-        combined=lists[0]
-        for i in range(1,len(lists)):
-            combined=mergeTwoLists(combined,lists[i])
-        return combined
-
+        combined=[]
+        for i in lists:
+            while i:
+                combined.append(i.val)
+                i=i.next
+                
+        combined=sorted(combined)
+        print(combined)
         
-       
+        dummy=node=ListNode(0,None)
+        for i in range(0,len(combined)):
+            node.next=ListNode(combined[i])
+            node=node.next
+            
+        return dummy.next
+            
+            
+        
