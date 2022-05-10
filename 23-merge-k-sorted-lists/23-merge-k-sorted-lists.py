d8e2ae48a04fe4c23cab31e5648cb8981c4b1a8c
Rakshit Parashar <34675136+rishu2403@users.noreply.github.com>
2022-05-09 20:40:43 -0700

    Time: 312 ms (15.03%), Space: 19 MB (9.55%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,3 +1,4 @@
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,21 +6,29 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        combined=[]
-        for i in lists:
-            while i:
-                combined.append(i.val)
-                i=i.next
-                
-        combined=sorted(combined)
-        print(combined)
-        
+        heap=[]
         dummy=node=ListNode(0,None)
-        for i in range(0,len(combined)):
-            node.next=ListNode(combined[i])
-            node=node.next
+        for i in range(0,len(lists)):
+            if lists[i]:
+                heapq.heappush(heap,(lists[i].val,i))
+
+                
+        # print(heap)        
+        while heap:
+            val,index=heapq.heappop(heap)
+            # print(val,index)
             
-        return dummy.next
+            dummy.next=ListNode(val)
             
+            dummy=dummy.next
             
+            if lists[index].next:
+                lists[index]=lists[index].next
+                heapq.heappush(heap,(lists[index].val,index))
+                print(heap)
+                
+                
+        return node.next
+        
+        
         
